32d73a392a1f83035786793029cd7bbfce22b5d4
assoron <ct.lohmann@posteo.de>
2020-08-14 17:57:37 +0200

    Add @quelgar's fix to interrupt blocking call

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,27 @@
-import zio. { ZIO, App, ZEnv, ExitCode }
+import zio. { ZIO, App, ZEnv, ExitCode, Chunk, ZLayer, Has, RIO, IO }
+
+import zio.console._
 
 import zio.nio._
-import zio.nio.channels._
+
+
+import zio.nio.core.channels._
+
+// import zio.nio.channels._
+
 
 import zio.nio.core._
 
 import zio.stream._
 
+import java.io.IOException
+
 object Gateway extends App {
+
+  def nioBlocking[A, C <: Channel](
+    channel: C
+  )(f: C => ZIO[zio.blocking.Blocking, Exception, A]): ZIO[zio.blocking.Blocking, Exception, A] =
+    zio.blocking.blocking(f(channel)).fork.flatMap(_.join).onInterrupt(channel.close.ignore)
   
   val programm = 
     ZStream.managed(server(8068))
@@ -16,8 +30,9 @@ object Gateway extends App {
 
   def server(port: Int) = {
     for {
+      server        <- DatagramChannel.open.toManaged_
       socketAddress <- SocketAddress.inetSocketAddress(port).option.toManaged_
-      server        <- DatagramChannel.bind(socketAddress)
+      _             <- server.bind(socketAddress).toManaged_
     } yield server
   } 
   
@@ -25,7 +40,7 @@ object Gateway extends App {
     ZStream.repeatEffect {
       for {
         buffer <- Buffer.byte(16)
-        _      <- server.receive(buffer)
+        _      <- nioBlocking(server)(_.receive(buffer))
         _      <- buffer.flip
         chunk  <- buffer.getChunk()
       } yield chunk
