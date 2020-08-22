ce549c6efdf686f3dedfe2158d232062943ce19c
Claas Lohmann <ct.lohmann@posteo.de>
2020-08-22 15:31:25 +0200

    Add EndpointRepository to Layer Cake

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,5 +1,4 @@
 import java.io.IOException
-
 import zio._
 import zio.clock.Clock
 import zio.{App, Schedule}
@@ -10,21 +9,30 @@ import zio.stream._
 import zio.duration._
 import zio.Layer
 
+
+
+
 class Port private(val number: Int) extends AnyVal {}
+
+sealed trait PortRangeException extends IOException
+case object UnexpectedPortRangeException extends PortRangeException
 
 object Port {
 
-  // TODO: proper errors
-  def apply(number: Int) =
-    Either.cond(number >= 0 && number < 65535, new Port(number), throw new Exception)
+  def apply(number: Int): Either[PortRangeException, Port] =
+    Either.cond(1 to 65535 contains number, new Port(number), UnexpectedPortRangeException)
+
 }
 
-object PortRepository {
+// TODO: Decide where to put this
 
-  type PortRepository = Has[PortRepository.Service]
+
+object ConfigRepository {
+
+  type ConfigRepository = Has[ConfigRepository.Service]
 
   trait Service {
-    def get(): IO[Exception, Port]
+    def getPrimaryUDPPort: IO[PortRangeException, Port]
   }
 
   sealed trait ConfigFailure
@@ -32,61 +40,78 @@ object PortRepository {
     final case class UnexpectedConfigFailure(err: Throwable) extends ConfigFailure
   }
 
-  // TODO: Needs a proper "No Config Found" error etc.
-  def get(): ZIO[PortRepository, Exception, Port] =
-    ZIO.accessM(_.get.get())
+  def getPrimaryUDPPort: ZIO[ConfigRepository, PortRangeException, Port] =
+    ZIO.accessM(_.get.getPrimaryUDPPort)
 
 }
 
-sealed trait Endpoint[R, E, A] {
+object ConfigRepositoryInMemory extends ConfigRepository.Service {
 
-  val create: ZStream[R, E, A]
+  // TODO: Configure a proper fallback
+  override def getPrimaryUDPPort: IO[PortRangeException, Port] = ZIO.fromEither(Port(9997025).orElse(Port(7025)))
 
+  def toLayer: Layer[Nothing, Has[ConfigRepository.Service]] = ZLayer.succeed(this)
 }
-case object WebSocketEndpoint extends Endpoint[Any, Exception, Nothing] {
 
-  lazy val create = ???
+//sealed trait Endpoint
+//case object WebSocketEndpoint extends Endpoint
+//case object DatagramEndpoint extends Endpoint
 
-}
 /*
- *   TODO: This might need to be changed to a case class to allow multiple sockets
- *   If done so move the Port or ConfigRepository outside but keep it in
+ * TODO: Wrapping this might be necessary to do proper apply() calls?
  */
-import PortRepository.PortRepository // TODO: Needs to be formatted asap!
-case object DatagramEndpoint extends Endpoint[PortRepository, Exception, DatagramChannel] {
+sealed trait Endpoint
+// final case class WebSocketEndpoint extends Endpoint
+final case class DatagramEndpoint(channel: DatagramChannelWrapper) extends Endpoint
+class DatagramChannelWrapper(val underlying: DatagramChannel) extends AnyVal
 
+object EndpointRepository {
+  import ConfigRepository.ConfigRepository
 
-  private val channel: ZManaged[PortRepository, Exception, DatagramChannel] =
-    for {
-      port          <- PortRepository.get().toManaged_
-      socketAddress <- SocketAddress.inetSocketAddress(port.number).option.toManaged_
-      channel       <- DatagramChannel.bind(socketAddress)
-    } yield channel
+  type EndpointRepository = Has[EndpointRepository.Service]
 
-  val create: ZStream[PortRepository, Exception, DatagramChannel] = ZStream.managed(channel)
+  trait Service {
+    def createDatagramEndpoint: ZManaged[EndpointRepository with ConfigRepository, IOException, DatagramChannel]
+  }
+
+  def createDatagramEndpoint: ZManaged[EndpointRepository with ConfigRepository, IOException, DatagramChannel] =
+    ZManaged.accessManaged(_.get.createDatagramEndpoint)
 
 }
 
-object PortRepositoryInMemory extends PortRepository.Service {
+object EndpointRepositoryInMemory extends EndpointRepository.Service {
+  import ConfigRepository.ConfigRepository
 
-  override def get() = ZIO.fromEither(Port(7025))
+  private val datagramChannel: ZManaged[ConfigRepository, IOException, DatagramChannel] =
+    for {
+    port          <- ConfigRepository.getPrimaryUDPPort.toManaged_
+    socketAddress <- SocketAddress.inetSocketAddress(port.number).option.toManaged_
+    channel       <- DatagramChannel.bind(socketAddress)
+  } yield channel
 
-  def toLayer: Layer[Nothing, Has[PortRepository.Service]] = ZLayer.succeed(this)
+  override def createDatagramEndpoint: ZManaged[ConfigRepository, IOException, DatagramChannel] = datagramChannel
+
+  def toLayer: ZLayer[ConfigRepository, IOException, Has[EndpointRepository.Service]] = ZLayer.succeed(this)
 }
 
 object Application extends App {
 
   val program =
     (for {
-      udp  <- DatagramEndpoint.create
+      udp  <- ZStream.managed(EndpointRepository.createDatagramEndpoint)
       send  = sender(udp).take(1)
       rece  = receiver(udp).take(1)
       _    <- ZStream.mergeAll(2, 16)(send, rece)
     } yield ()).runDrain
 
-  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    program.provideCustomLayer(PortRepositoryInMemory.toLayer).tap(l => putStrLn(l.toString)).orDie.exitCode
-  }
+  val layer = ConfigRepositoryInMemory.toLayer >+> EndpointRepositoryInMemory.toLayer
+
+  def run(args: List[String]): URIO[zio.ZEnv with Console, ExitCode] =
+    program
+      .tap(l => putStrLn(l.toString))
+      .provideCustomLayer(layer)
+      .orDie
+      .exitCode
 
   def sender(stream: DatagramChannel) =
     ZStream(stream).flatMap(sendDatamgrams)
@@ -114,6 +139,34 @@ object Application extends App {
       } yield i
     }, Schedule.fixed(1.second) ++ Schedule.recurs(1))
 }
+
+//sealed trait Endpoint[R, E, A] {
+//
+//  val create: ZStream[R, E, A]
+//
+//}
+//case object WebSocketEndpoint extends Endpoint[Any, Exception, Nothing] {
+//
+//  lazy val create = ???
+//
+//}
+//
+//import ConfigRepository.ConfigRepository // TODO: Needs to be formatted asap!
+//case object DatagramEndpoint extends Endpoint[ConfigRepository, Exception, DatagramChannel] {
+//
+//  private val channel: ZManaged[ConfigRepository, Exception, DatagramChannel] =
+//    for {
+//      port          <- ConfigRepository.getPrimaryUDPPort.toManaged_
+//      socketAddress <- SocketAddress.inetSocketAddress(port.number).option.toManaged_
+//      channel       <- DatagramChannel.bind(socketAddress)
+//    } yield channel
+//
+//  val create: ZStream[ConfigRepository, Exception, DatagramChannel] = ZStream.managed(channel)
+//
+//}
+
+
+
 
 
 /*
