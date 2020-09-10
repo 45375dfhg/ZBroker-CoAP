a14c7439e1ff9dcef954c8a382192c3483d3e5aa
assoron <ct.lohmann@posteo.de>
2020-09-10 15:55:03 +0200

    Refactor CoapPayloadMediaTypes into CoapPayload.scala

--- src/main/scala/domain/model/coap/CoapPayload.scala
+++ src/main/scala/domain/model/coap/CoapPayload.scala
@@ -11,4 +11,18 @@ object TextCoapPayloadContent {
   def apply(chunk: Chunk[Byte]): TextCoapPayloadContent = new TextCoapPayloadContent(chunk.map(_.toChar).mkString)
 }
 
+/**
+ * The Media Format of an
+ */
+sealed trait CoapPayloadMediaTypes
+case object TextMediaType        extends CoapPayloadMediaTypes
+case object LinkMediaType        extends CoapPayloadMediaTypes
+case object XMLMediaType         extends CoapPayloadMediaTypes
+case object OctetStreamMediaType extends CoapPayloadMediaTypes
+case object EXIMediaType         extends CoapPayloadMediaTypes
+case object JSONMediaType        extends CoapPayloadMediaTypes
+
+// DEFAULT FALLBACK
+case object SniffingMediaType    extends CoapPayloadMediaTypes
+
 // TODO: Implement the other Media Types
