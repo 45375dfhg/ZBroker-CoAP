f7bb7ad13611a024055907fa9df45e6a6d11871c
Claas Lohmann <ct.lohmann@posteo.de>
2020-10-10 21:03:38 +0200

    Add DuplicateRejectionService.scala

---
+++ src/main/scala/DuplicateRejectionService.scala
@@ -0,0 +1,24 @@
+
+import domain.model.dupetracker.DuplicationTrackerRepository
+import domain.model.dupetracker.DuplicationTrackerRepository._
+
+import zio._
+import zio.clock._
+import zio.duration._
+
+object DuplicateRejectionService {
+
+  /**
+   * Adds an element of A to the underlying collection and removes it afterwards.
+   * @param element An element of A that is to be added fleetingly to the underlying collection.
+   * @param n The number of seconds of delay to the removal.
+   * @return A Boolean value which represents whether an element was added (and removed).
+   *         true means it was added and WILL be removed (after n-seconds).
+   */
+  def addAndDeleteAfter[A](element: A, n: Int = 145): URIO[DuplicationTrackerRepository[A] with Clock, Boolean] =
+    for {
+      contains <- DuplicationTrackerRepository.add(element)
+      _        <- DuplicationTrackerRepository.remove(element).unless(contains).delay(n.seconds).fork
+    } yield contains
+
+}
