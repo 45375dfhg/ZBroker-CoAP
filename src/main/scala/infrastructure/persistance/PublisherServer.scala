07a0212d389568f319b3659cca4f3da0adfc7414
Claas Lohmann <ct.lohmann@posteo.de>
2020-11-02 12:06:06 +0100

    Add comments to refactored PublisherServer

--- src/main/scala/infrastructure/persistance/PublisherServer.scala
+++ src/main/scala/infrastructure/persistance/PublisherServer.scala
@@ -32,15 +32,22 @@ object PublisherServer {
       _ <- ChunkStreamRepository.getChunkStream.mapMParUnordered(n.value)(operation).runDrain
     } yield ()
 
-
+  /**
+   * Entry point that depending on tuple will attempt to send a reset message or attempt to send
+   * an acknowledgment message and then attempt to publish the content
+   * @param element a tuple based on the origins address and the received datagram
+   */
   private def operation(element: (Option[SocketAddress], Chunk[Byte])) =
     CoapDeserializerService.parseCoapMessageWithoutErr(element._2).flatMap {
       case Left(id) => attemptReset(element._1, id)
       case Right(m) => attemptAcknowledgment(element._1, m) *> attemptPublish(m)
     }.ignore
 
-
   // TODO: MessageSenderRepository needs a service!
+  /**
+   * Attempts to send a reset message if the origin's socket address is defined and a message id was
+   * successfully recovered from the datagram - else the respective successful error is returned
+   */
   private def attemptReset(address: Option[SocketAddress], id: Option[CoapId]) =
     ((address, id) match {
       case (Some(addr), Some(id)) => MessageSenderRepository.sendMessage(addr, ResponseService.getResetMessage(id))
@@ -48,6 +55,10 @@ object PublisherServer {
       case (Some(_), None)        => IO.fail(MissingCoapId)
     }).ignore
 
+  /**
+   * Attempts to send an acknowledgment message to the origin if it is defined and the origin requires a response,
+   * otherwise the respective successful error is returned
+   */
   private def attemptAcknowledgment(address: Option[SocketAddress], msg: CoapMessage) =
     (address match {
       case None       => IO.fail(MissingAddress)
@@ -55,15 +66,21 @@ object PublisherServer {
         IO.cond(msg.isConfirmable, ResponseService.getAckMessage(msg), NoResponseAvailable) >>= (sendMessage(addr, _))
     }).ignore
 
+  /**
+   * If a message has a PUT code, an URI-path and a payload, the message will be published on the broker
+   */
   private def attemptPublish(m: CoapMessage): URIO[BrokerRepository[PublisherResponse], Unit] =
     (for {
-      _       <- isPut(m)
+      _       <- hasPutMessageCode(m)
       path    <- m.getPath
       content <- m.getContent
       _       <- BrokerRepository.pushMessageTo(path, PublisherResponse.from(path, content))
     } yield ()).ignore
 
-  private def isPut(msg: CoapMessage): IO[UnsharablePayload, Unit] =
+  /**
+   * Checks the header of a message for its code - if the code is 0:3 (PUT) it returns true
+   */
+  private def hasPutMessageCode(msg: CoapMessage): IO[UnsharablePayload, Unit] =
     IO.cond(
       msg.header.coapCodePrefix.value == 0 && msg.header.coapCodeSuffix.value == 3,
       (),
